23f347c9b6263656c5260be0672435309a20e5a8
fitzpben <benfitzpatrick1@gmail.com>
2025-08-09 17:30:24 +0100

    feat: asteroids

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
-    numpass, numfail = pygame.init()
+    pygame.init()
+
+    gameClock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    p = Player(SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2))
+    a = AsteroidField()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -14,7 +35,27 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
+        for ast in asteroids:
+            for s in shots:
+                asteroid_shot_bool = ast.checkCollision(s)
+                if asteroid_shot_bool:
+                    s.kill()
+                    ast.split()
+
+            player_collision_bool = ast.checkCollision(p)
+            if player_collision_bool:
+                print ("Game over!")
+                sys.exit()
+
+
         pygame.display.flip()
+        timePassed = gameClock.tick(60)
+        dt = timePassed / 1000
 
 if __name__ == "__main__":
     main()
